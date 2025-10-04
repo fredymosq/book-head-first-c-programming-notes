8c39a4fafd577545f780e90d6d9223ac4e932ced
Fredy Mosquera <25505457+fmosquera0101@users.noreply.github.com>
2025-10-04 10:12:45 -0500

    adding excersice 1 chapter 1

--- chapter1/cards.c
+++ chapter1/cards.c
@@ -8,23 +8,49 @@ int getValUsingSwitch(char card_name[3]);
 int main(){
 
     char card_name[3];
-    puts("Enter the card_name:");
-    scanf("%2s", card_name);
-
-    int val = getValUsingSwitch(card_name) ;
-
-
-
-    if ((val >= 2) && (val <= 6))
+    int count = 0;
+    while (card_name[0] != 'X')
     {
-        puts("Count has gone up");
-    }else
-    {
-        puts("Count has gone down");
+        puts("Enter the card_name:");
+        scanf("%2s", card_name);
+        
+        int val = 0;
+        
+        switch (card_name[0])
+        {
+            case 'K':
+            case 'Q':
+            case 'J':
+                val = 10;
+                break;
+
+            case 'A':
+                val = 11;
+                break;
+    
+            default:
+                val = atoi(card_name);
+                if ((val < 1) || (val > 10))
+                {
+                    puts("I don't understand that value");
+                    continue;
+                }
+
+        }
+
+        if ((val > 2) && (val < 7))
+        {
+            count++;
+        }else if (val == 10)
+        {
+            count--;
+        }
+
+        printf("Current count: %i\n", count);
+
     }
     
-    
-
+  
     return 0;
 }
 
