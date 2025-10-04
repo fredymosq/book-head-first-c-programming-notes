4123be68d8c42c02f0d819d175f38d1ca0d544a9
Fredy Mosquera <25505457+fmosquera0101@users.noreply.github.com>
2025-10-04 09:58:34 -0500

    adding excersice 1 chapter 1

---
+++ chapter1/exercise1.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int x = 0;
+    int y = 0;
+    while (x < 5)
+    {
+        
+        //y = x - y;
+
+        //y = x + y;
+
+        // y = y + 2;
+        // if(y > 4){
+        //     y = y -1;
+        // }
+
+        // x = x + 1;
+        // y = y + x;
+
+        if (y < 5)
+        {
+            x = x + 1;
+            if (y < 3)
+            {
+                x = x - 1;
+            }
+            
+        }
+         y = y + 2;
+        
+        printf("%i%i ", x, y);
+
+        x = x + 1;
+    }
+    
+    return 0;
+}
